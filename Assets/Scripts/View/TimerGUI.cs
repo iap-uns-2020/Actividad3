42eae6192be28dc309463806fa5fdfd4c1b89117
Bárbara Mattioli <48759643+Barbmatt@users.noreply.github.com>
2020-11-19 22:00:21 -0300

    Timer

---
+++ Assets/Scripts/View/TimerGUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private DateTime startTime;
+    private DateTime finnishTime;
+    public Text textTime;
+
+    public void RegisterStartTime()
+    {
+        startTime = System.DateTime.Now;
+    }
+
+    public void RegisterFinnishTime()
+    {
+        finnishTime = System.DateTime.Now;
+    }
+
+    public void ShowTimeSpendInLevel()
+    {
+        TimeSpan duration = startTime - finnishTime;
+        textTime.text = duration.ToString();
+    }
+
+}
