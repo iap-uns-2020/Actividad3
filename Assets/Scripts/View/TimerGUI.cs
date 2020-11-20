8d8f2ec46974430ae7b04bd955757508142675ae
Bárbara Mattioli <48759643+Barbmatt@users.noreply.github.com>
2020-11-19 22:29:53 -0300

    asociacion de botones al script de timer

--- Assets/Scripts/View/TimerGUI.cs
+++ Assets/Scripts/View/TimerGUI.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using System;
 using System.IO;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TimerGUI : MonoBehaviour
 {
     private DateTime startTime;
     private DateTime finnishTime;
