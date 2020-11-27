8c21317ce6c0c7a9eca76c1eedbaae2f7bbf66f8
gsagergithub <63723756+gsagergithub@users.noreply.github.com>
2020-11-27 15:23:11 -0300

    Agregado: notificaciones de fin de juego al chocar contra la meta y los huecos

--- Assets/Scripts/View/SceneObjectCollided.cs
+++ Assets/Scripts/View/SceneObjectCollided.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneObjectCollided : MonoBehaviour, Collidable
 {
     private GameManagerGUI gameManagerGUI;
+    public GameObject winPanel;
+    public GameObject losePanel;
 
-    public void ActionObjectCollided()
-    {
+    public void ActionObjectCollided(){
         MessagesCollisionDictionary messagesCollisionDictionary = new MessagesCollisionDictionary();
-
         string nameObjectCollided = transform.name;
-
         Dictionary<string, string> messages = messagesCollisionDictionary.SetMessages();
-
         string messageCollision = messagesCollisionDictionary.GetMessage(nameObjectCollided);
-
-        Debug.Log(messageCollision);
+        
+        if(string.Compare(messageCollision, "You Won!")==0)
+            Instantiate(winPanel);
+        else
+            Instantiate(losePanel);
+        
         //gameManagerGUI.GameResult(messageCollision);
+
     }
 }
