5f6eccc3b9d42f24a78411be22e9199250b75a3c
gsagergithub <63723756+gsagergithub@users.noreply.github.com>
2020-12-01 15:12:46 -0300

    Agregado: Introducción a la aplicación.

---
+++ Assets/Scripts/OpeningManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpeningManager : MonoBehaviour
+{
+	public Text waitText;
+	private int i=0;
+	private const int MAINMENUSCENE = 0;
+    void Start(){
+       StartCoroutine(OpeningAnimation()); 
+    }
+
+    private IEnumerator OpeningAnimation(){
+    
+    	for(int i=0; i<3; i++){
+    		waitText.text+='.';	
+    		yield return new WaitForSeconds(1);
+    	} 
+    	SceneManager.LoadScene(MAINMENUSCENE);  
+    }
+
+}
