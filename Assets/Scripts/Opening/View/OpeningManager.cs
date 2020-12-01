f37d2233298a04f5e6852cc9b8af3a6cd3b2a8c1
gsagergithub <63723756+gsagergithub@users.noreply.github.com>
2020-12-01 15:27:07 -0300

    Agregado: Interfaz IOpeningManager

---
+++ Assets/Scripts/Opening/View/OpeningManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Opening{
+	public class OpeningManager : MonoBehaviour, IOpeningManager{
+		private const int MAINMENUSCENE = 0;
+		private const int SECONDSTOWAIT = 1;
+		public Text waitText;
+	    void Start(){
+	       StartCoroutine(OpeningAnimation()); 
+	    }
+
+	    private IEnumerator OpeningAnimation(){
+	    
+	    	for(int i=0; i<3; i++){
+	    		waitText.text+='.';	
+	    		yield return new WaitForSeconds(SECONDSTOWAIT);
+	    	} 
+	    	SceneManager.LoadScene(MAINMENUSCENE);  
+	    }
+
+	}	
+}
+
